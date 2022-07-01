193044e8f8ea1f0fd485bcbf544836551fe3d960
designsh <design__sh@naver.com>
2022-07-01 16:04:07 +0900

    신종하 : 업데이트

--- GameEngine/PreCompile.cpp
+++ GameEngine/PreCompile.cpp
@@ -13,4 +13,5 @@
 #pragma comment(lib, "zlib-md.lib")			// 문제없음
 #pragma comment(lib, "libxml2-md.lib")		// 문제없음
 //#pragma comment(lib, "libfbxsdk.lib")		// 특정 static 함수 링크에러발생
-#pragma comment(lib, "libfbxsdk-md.lib")	// 메모리누수발생
+#pragma comment(lib, "libfbxsdk-md.lib")	// 메모리누수발생(visual studio 2022(fbx sdk v2014)과 fbx sdk v2019의 버젼차이로 메모리릭이발생)
+
